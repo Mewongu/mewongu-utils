b5f4251c99b16212dbda2e12058d828febb64bdf
Andreas Stenberg <andreas@Andreass-MacBook-Pro.local>
2017-08-22 22:50:35 +0200

    Created a function for tailing files

--- mewongu_utils/__init__.py
+++ mewongu_utils/__init__.py
@@ -1,8 +1,9 @@
 # mewongu_utils
 # Author: Andreas Stenberg
 
-from time import time
+from time import time, sleep
 from functools import wraps
+import os
 
 
 class timed_ctx:
@@ -10,6 +11,10 @@ class timed_ctx:
     Context manager for timing a block of code
     """
     def __init__(self, msg='Elapsed time:', log_func=None):
+        """
+        :param msg: The message to display before the time
+        :param log_func: The function to call with the full message. Must handle a single string argument
+        """
         self.log_func = log_func
         self.msg = msg
 
@@ -28,6 +33,10 @@ class timed:
     Decorator for timing a function call
     """
     def __init__(self, msg='Elapsed time:', log_func=None):
+        """
+        :param msg: The message to display before the time
+        :param log_func: The function to call with the full message. Must handle a single string argument
+        """
         self.log_func = log_func
         self.msg = msg
 
@@ -38,3 +47,28 @@ class timed:
                 return fn(*args, **kwargs)
         return fn_call
 
+
+def tail(filename, check_interval=1):
+    """
+    Tail a file checking for changes
+    :param filename: The file to tail
+    :param check_interval: How often should it be rechecked? default: 1 second
+    :return: yields the file line by line
+    """
+    if not os.path.exists(filename):
+        raise FileNotFoundError(filename)
+    last_pos = 0
+    while True:
+        try:
+            with open(filename, 'r') as fin:
+                fin.seek(last_pos)
+                res = True
+                while res:
+                    res = fin.readline().rstrip()
+                    if res:
+                        yield res
+                last_pos = fin.tell()
+        except FileNotFoundError:
+            pass
+        finally:
+            sleep(check_interval)
